38b391b0285d2e473564564a3a55077258d00e16
lhofmann <lhofmann@users.noreply.github.com>
2018-11-18 18:06:03 +0100

    Tests for C++ parser

--- tests/test_it.py
+++ tests/test_it.py
@@ -1,7 +1,6 @@
 import os
 import pvxmlgen
 import xml.etree.ElementTree as ET
-from functools import cmp_to_key
 from glob import glob
 import difflib
 
@@ -34,3 +33,29 @@ def test_parsing():
         xml = pvxmlgen.generate_xml(contents)
         ground_truth = ET.parse(filename + '.xml').getroot()
         assert ComparableElement(xml) == ComparableElement(ground_truth)
+
+
+def test_cpp_parsing():
+    def output(type_, name, num_elements, defaults):
+        return {
+            'name': name,
+            'type': type_,
+            'number_of_elements': num_elements,
+            'default_values': defaults
+        }
+
+    test_cases = [
+        ('  double   x   = 3;', output('double', 'x', 1, 3)),
+        ('  double   x   { 3 } ;', output('double', 'x', 1, 3)),
+        ('  long int  my_var[5]   { 3, 4, 5, 6, 7 } ;', output('long int', 'my_var', 5, (3, 4, 5, 6, 7))),
+        ('  bool b = false;', output('bool', 'b', 1, False)),
+        (' \t\t bool \tvar\t  {true } ;', output('bool', 'var', 1, True)),
+        ('  float fs[3] = {1.0, 2, 3.0};', output('float', 'fs', 3, (1.0, 2, 3.0))),
+        (' class X', {'class': 'X'}),
+        (' class myclass : public Y, private Z', {'class': 'myclass'}),
+        (' class __attribute__ ((visibility("default"))) myclass {', {'class': 'myclass'}),
+        ('  class some_cls : public other {  ', {'class': 'some_cls'}),
+    ]
+
+    for string, result in test_cases:
+        assert pvxmlgen.parse_declaration(string) == result
