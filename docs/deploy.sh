36e98d5a2799bce97b18d3f5e09bc554c12c2745
lhofmann <lhofmann@users.noreply.github.com>
2018-11-18 12:32:05 +0100

    Travis builds and deploys sphinx docs

---
+++ docs/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd ..
+git clone -b gh-pages "https://$GH_TOKEN@github.com/lhofmann/pvxmlgen.git" gh-pages
+cd gh-pages
+
+git config user.name "Travis Builder"
+git config user.email "lhofmann@users.noreply.github.com"
+
+cp -R ../pvxmlgen/docs/build/html/* ./
+
+git add -A .
+git commit -m "[ci skip] Autodoc commit for $COMMIT."
+git push -q origin gh-pages
